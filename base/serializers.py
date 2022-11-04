dd73aed090b6e1d05dee38ac42d44b882de8ba08
Mikhail Efremov <maefremov@avito.ru>
2022-11-04 20:39:16 +0300

    Change serializer

--- base/serializers.py
+++ base/serializers.py
@@ -2,25 +2,25 @@ from base.models import *
 from rest_framework import serializers
 
 
-class LinkTagSerializer(serializers.HyperlinkedModelSerializer):
+class LinkTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = LinkTag
         fields = '__all__'
 
 
-class SourceTagSerializer(serializers.HyperlinkedModelSerializer):
+class SourceTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = SourceTag
         fields = '__all__'
 
 
-class LinkSerializer(serializers.HyperlinkedModelSerializer):
+class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
         fields = '__all__'
 
 
-class SourceSerializer(serializers.HyperlinkedModelSerializer):
+class SourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
         fields = '__all__'
